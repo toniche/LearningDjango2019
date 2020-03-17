4cde3ea9979cc18664b40c9c22b6ff978d9a6817
toni lolo <toni@agsatpc.com>
2020-03-17 14:37:28 +0100

    layout.html

--- LearningDjango2019/HelloDjangoApp/views.py
+++ LearningDjango2019/HelloDjangoApp/views.py
@@ -17,3 +17,12 @@ def index(request):
     )
 
    
+def about(request):
+    return render(
+        request,
+        "HelloDjangoApp/about.html",
+        {
+            'title' : "About HelloDjangoApp",
+            'content' : "Example app page for Django."
+        }
+    )
--- LearningDjango2019/LearningDjango2019/urls.py
+++ LearningDjango2019/LearningDjango2019/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     #path('admin/', admin.site.urls)
     url(r'^$', HelloDjangoApp.views.index, name='index'),
     url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    url(r'^about$', HelloDjangoApp.views.about, name='about'),
 ]

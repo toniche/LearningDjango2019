398715beff849ba19a77faa9cfb10f62b64df108
toni lolo <toni@agsatpc.com>
2020-03-17 11:41:56 +0100

    Template html

--- LearningDjango2019/LearningDjango2019/settings.py
+++ LearningDjango2019/LearningDjango2019/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'HelloDjangoApp',
 ]
 
 # Middleware framework
@@ -101,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
